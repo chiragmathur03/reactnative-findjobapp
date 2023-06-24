4bd0ca778239f871bfbf9b398c0db03891aafa52
Chirag Mathur <54184768+chiragmathur03@users.noreply.github.com>
2023-06-24 11:47:44 +0530

    initial commit

---
+++ app/_layout.js
@@ -0,0 +1,30 @@
+// To link to all other pages in our application
+
+import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+    // Load the fonts
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    // Show the splashscreen until the fonts loaded - Think like a useEffect Hook
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if (!fontsLoaded) return null;
+
+    return <Stack onLayout={onLayoutRootView} />
+}
+
+export default Layout;
---
+++ utils/index.js
@@ -0,0 +1,8 @@
+// A Utility Function which checks the image url of the company in popular job sessions. Does it exist or not
+export const checkImageURL = (url) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        return pattern.test(url);
+    }
+}; 
